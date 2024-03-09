5a4cfe13866873ac15b7d1e0748ddc31f87f4542
revesfideles <revesfideles@gmail.com>
2024-03-09 09:10:35 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Code your solution here
+const drivers = [
+     {
+        firstName: "Bobby",
+        homeTown: "Pittsburgh"
+    },
+     { 
+        firstName: "Sammy",
+        homeTown: "New York"
+    },
+     {  
+        firstName: "Sally",
+        homeTown: "Cleveland"
+    },
+     { 
+        firstName: "Annette",
+        homeTown: "Los Angeles"
+    },
+     { 
+        firstName: "Sarah",
+        homeTown: ""
+    },
+     {   
+        firstName: "bobby",
+        homeTown: "TampaBay"
+    }
+
+];
+
+function findMatching(drivers, query) {
+    const lowercaseQuery = query.toLowerCase();
+    const matchingDriverNames = drivers
+      .filter(driver => driver.toLowerCase() === lowercaseQuery);
+    return matchingDriverNames;
+  }
+
+function fuzzyMatch(drivers, query) {
+   const lowercaseQuery = query.toLowerCase();
+   const matchingDrivers = drivers.filter(driver => driver.toLowerCase().startsWith(lowercaseQuery));
+   return matchingDrivers;
+}
+
+function matchName(drivers, query) {
+    const matchingDrivers = drivers.filter(driver => driver.name === query);
+    return matchingDrivers;
+  }
